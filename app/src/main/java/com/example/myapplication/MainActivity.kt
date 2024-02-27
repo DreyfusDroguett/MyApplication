f20c748cb0d9d931c9e5e7f0726a9bbd8ef4f047
Dreyfus <Dreyfus.assis@gmail.com>
2024-02-27 13:36:39 -0300

    Gerador

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+
+    fun sortear(view: View){
+
+            val textoResultado = findViewById<TextView>(R.id.txt_resultado)
+           val numero = java.util.Random().nextInt(11)
+            textoResultado.setText("número gerado $numero")
+
+
+    }
+
+
+
+}
